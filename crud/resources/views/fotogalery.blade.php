5a16c2d6cb4fe6dc55d2363456ccf573b4ae2dd3
toronnaro <nottoro2@gmail.com>
2024-03-18 10:25:19 +0800

    crud

---
+++ crud/resources/views/fotogalery.blade.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>crud</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <h1 class="text-center mb-4">Galery</h1>
+
+    <div class="container">
+        <button type="button" class="btn btn-success">Tambah +</button>
+        <div class="row">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">No</th>
+                        <th scope="col">Nama</th>
+                        <th scope="col">Deskripsi</th>
+                        <th scope="col">foto</th>
+                        <th scope="col">select</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($data as $row)
+                        <tr>
+                            <th scope="row">{{ $row->id }}</th>
+                            <td>{{ $row->nama }}</td>
+                            <td>{{ $row->deskripsi }}</td>
+                            <td>bruh</td>
+                            <td>
+                                <button type="button" class="btn btn-danger">Hapus</button>
+                                <button type="button" class="btn btn-info">Edit</button>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
